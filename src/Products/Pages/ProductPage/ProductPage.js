ef1bd6ef6ea04f98f5fc7df382ed5333cb019386
Mohammed Rezq <mohammedrezq2000@gmail.com>
2020-09-27 05:24:59 +0200

    Added Accordion and Radio Buttons to Product Page

--- src/Products/Pages/ProductPage/ProductPage.js
+++ src/Products/Pages/ProductPage/ProductPage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+/* Material UI Components */
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 // import ProductGallery from "../../Components/ProductGallery/ProductGallery";
-import Button from '../../../Shared/UIElements/Button'
+import Button from "../../../Shared/UIElements/Button";
+import "./ProductPage.css";
 
 /* Product Size we should add product size based on its type whether its shoes or clothes */
 const DUMMY_SINGLE_PRODUCT = {
@@ -17,16 +25,19 @@ const DUMMY_SINGLE_PRODUCT = {
     "Model is wearing size S and is 5'9\"/175cm",
     "Loose fit for a roomy feel",
   ],
-  productImages: ['https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/0a6391bb-dd85-4b99-a3b1-93d29e6e0c2e/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg','https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/b7051cb5-5966-439b-950a-df231c4363ec/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg','https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/13e1826e-2282-4123-9975-32b6bf25a728/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg']
+  productImages: [
+    "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/0a6391bb-dd85-4b99-a3b1-93d29e6e0c2e/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg",
+    "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/b7051cb5-5966-439b-950a-df231c4363ec/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg",
+    "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/13e1826e-2282-4123-9975-32b6bf25a728/yoga-luxe-womens-infinalon-crop-top-5XGffB.jpg",
+  ],
 };
 
 const ProductPage = (props) => {
+  const [expanded, setExpanded] = useState(false);
 
-    // const [isOpen, setIsOpen] = useState(false);
-
-    // const openCollapseonClick = () => {
-    //     setIsOpen(!isOpen);
-    // }
+  const handleAccordionChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
     <>
@@ -35,27 +46,69 @@ const ProductPage = (props) => {
         <h1>{DUMMY_SINGLE_PRODUCT.productSeries}</h1>
         <div>${DUMMY_SINGLE_PRODUCT.productPrice}</div>
       </div>
-      {DUMMY_SINGLE_PRODUCT.productImages.map(productImage => <img style={{maxHeight: "500px"}} src={productImage} alt={DUMMY_SINGLE_PRODUCT.productTitle} />)}
-      
+      {DUMMY_SINGLE_PRODUCT.productImages.map((productImage) => (
+        <img
+          style={{ maxHeight: "500px" }}
+          src={productImage}
+          alt={DUMMY_SINGLE_PRODUCT.productTitle}
+        />
+      ))}
+
       <div className={`product-extra-information`}>
-          {/* Change radio btns into Buttons https://stackoverflow.com/questions/16242980/making-radio-buttons-look-like-buttons-instead */}
-          {DUMMY_SINGLE_PRODUCT.productSizes.map(productSize => 
-        <div className={`product-size`}>
-            <label htmlFor={DUMMY_SINGLE_PRODUCT.productId} className={`radioSizeCSS`}>
-          <input id={DUMMY_SINGLE_PRODUCT.productId} name="SKUandSize" type="radio" className="sizeProduct" />
-          {<i>${productSize}</i>}</label>
-        </div>
-            )}
+        {/* Change radio btns into Buttons https://stackoverflow.com/questions/16242980/making-radio-buttons-look-like-buttons-instead */}
+        {DUMMY_SINGLE_PRODUCT.productSizes.map((productSize, index) => (
+          <div className={`product-size selection-radio-btn`}>
+            <input
+              id={index}
+              name="SKUandSize"
+              type="radio"
+              className={`sizeProduct radioInput`}
+            />
+            <label htmlFor={index} className={`radioSizeCSS radioLabel`}>
+              {productSize}
+            </label>
+          </div>
+        ))}
       </div>
       <Button size="big">Add To Bag</Button>
       <Button size="big">Favorites</Button>
       {/* Add Collapsibles from Material UI */}
-            <div>
-                <p>
-                    {DUMMY_SINGLE_PRODUCT.productShipping}
-                </p>
-            </div>
+      <div>
+        <p>{DUMMY_SINGLE_PRODUCT.productShipping}</p>
+      </div>
 
+      {/* Material UI Accordion -Controlled- Rendering https://material-ui.com/components/accordion/#controlled-accordion */}
+      <div className={`product-page-accordions`} style={{ width: "30%" }}>
+        <Accordion expanded={expanded === 'panel1'} onChange={handleAccordionChange('panel1')} style={{ margin: "0" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon style={{ margin: "0" }} />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography>Size&Fit</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {/* render each ProductSizeFit Tip in one listitem (Dynamically) */}
+              {DUMMY_SINGLE_PRODUCT.productSizeFit.map(productItem => (<li>{productItem}</li>))}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === 'panel2'} onChange={handleAccordionChange('panel2')} style={{ margin: "0" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon style={{ margin: "0" }} />}
+            aria-controls="panel3a-content"
+            id="panel3a-header"
+          >
+            <Typography>Free Shipping & Returns</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              {DUMMY_SINGLE_PRODUCT.productShipping}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     </>
   );
 };
