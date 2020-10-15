62beb6d1621900ae0cfd7d0a770a646704cff408
Mohammed Rezq <mohammedrezq2000@gmail.com>
2020-10-15 16:20:37 +0200

    ADD TO CART FROM PRODUCT PAGE

--- src/Products/Pages/CartPage/CartPage.js
+++ src/Products/Pages/CartPage/CartPage.js
@@ -1,32 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link, useParams, useLocation, useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
-// import { connect } from 'react-redux';
+import Message from '../../../Shared/UIElements/Message';
+import Spinner from '../../../Shared/UIElements/Spinner';
+import { addToCart } from '../../../Store/Actions/cartActions';
+
 
 const CartPage = (props) => {
-    console.log('CART PAGE PROPS', props);
 
-    // let productsInCart = [];
-    // Object.keys(props.productsNumber.products).map( (item) => {
-    //     console.log("ONE PRODUCT FROM CART PAGE" ,props.productsNumber.products[item])
-    // })
+    const productId = useParams().id;
+    console.log(productId)
+
+    const qty = Number(useLocation().search.split("?")[2].split("=")[1]);
+    const size = useLocation().search.split("?")[1].split("=")[1];
+    // console.log(qty);
+    // console.log(size);
+    // console.log('CART PAGE PROPS', props);
+
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
+
+    console.log(cartItems)
+
+    const dispatch = useDispatch()
 
 
+    useEffect(() => {
+        if(productId);
+        dispatch(addToCart(productId, qty, size))
+    },[dispatch, productId, qty, size])
 
     return(
-    <h1>CartPage:</h1>
+    <h1>CartPage: </h1>
     );
 }
 
-// const mapStateToProps = state => {
-//     return {
-//         productsNumber: state.addtoCartReducer
-//       }
-// }
 
-// mapDispatchToProps = dispatch => {
-//     return {
-//         getProductsNumber: () => dispatch(actions.getProductsNumber())
-//       }
-// }
 
 export default CartPage;
